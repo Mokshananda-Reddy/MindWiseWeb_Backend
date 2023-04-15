adcee5de8b50a52350a21e32db0c7592f8002b69
MokshanandaReddy <mokshananda14102001@gmail.com>
2023-04-16 00:05:55 +0530

    initial commit

---
+++ src/main/java/com/mindwise/backend/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.mindwise.backend.repository;
+
+import com.mindwise.backend.model.Admin;
+import com.mindwise.backend.model.Doctor;
+import com.mindwise.backend.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+}
